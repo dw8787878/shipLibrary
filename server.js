2e54b2e3df31dba4f7a5d6ed19d64bd1db557b56
dw8787878 <darryn8wu@gmail.com>
2019-01-20 22:38:24 -0500

    chore: added express, settup basic routes

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const http = require('http');
-const hostname ='127.0.0.1';
+const express = require('express');
+const bodyParser = require('body-parser');
+const {body, validate} = require('express-validator/check');
+const app = express();
+const path = require('path');
+
+//const hostname ='127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
-});
+app.get('/', (req, res) => res.sendFile(resolve(__dirname, '..', '/Users/darrynwu/shipLibrary/public', 'index.html')));
 
-server.listen(port, hostname, () =>{
-  console.log(`Arcada Server Online!  Listening on port: ${port} at http://${hostname}`)
+app.listen(port, (err)=>{
+  if (err) {
+    return console.log('Arcada Server Crash!', err);
+  }
+  console.log(`Arcada Server Online!  Running local at port: ${port} is in directory: ${__dirname}`);
 });
