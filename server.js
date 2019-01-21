69b830638beb4923a645b01e6b3ab4a3071f2a31
dw8787878 <darryn8wu@gmail.com>
2019-01-21 08:52:18 -0500

    fix: Webpack, Babel, & React configurations fixed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 //const hostname ='127.0.0.1';
 const port = 3000;
 
-app.get('/', (req, res) => res.sendFile(resolve(__dirname, '..', '/Users/darrynwu/shipLibrary/public', 'index.html')));
+app.get('/', (req, res) => res.sendFile(`${__dirname}/public/`, 'index.html'));
 
 app.listen(port, (err)=>{
   if (err) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 module.exports = {
+  entry: './app/index.jsx',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: 'babel-loader' // config in .babelrc
       }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devServer: {
+    contentBase: './public'
   }
 };
